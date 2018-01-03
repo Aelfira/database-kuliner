f0a6b0732de3c1cad4748bd4f1062075ab2b637d
Bayu Fanani <fanani707@gmail.com>
2018-01-04 00:18:05 +0700

    buat kelas oci

--- admin/koneksi.php
+++ admin/koneksi.php
@@ -1,3 +1,25 @@
 <?php 
-mysql_connect("localhost", "root", "") or die(mysql_error());
-mysql_select_db("kuliner") or die(mysql_error());
+/* mysql_connect("localhost", "root", "") or die(mysql_error());
+mysql_select_db("kuliner") or die(mysql_error()); */
+class oci {
+	private $koneksi = '';
+	private $user = 'amalia';
+	private $password = '1234';
+	private $db = 'localhost/XE';
+	private $query = NULL;
+
+	function __construct() {
+		$this->koneksi = oci_connect($this->user, $this->password, $this->db);
+	}
+
+	function query($sql)
+	{
+		$this->query = oci_parse($this->koneksi, $sql) or die(oci_error());
+		return oci_execute($this->query);
+	}
+
+	function fetch_array()
+	{
+		return oci_fetch_array($this->query);
+	}
+}
